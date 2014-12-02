54c8a43297d2590da20d4a3a3bbc7b8a3f58b65d
patcharavtk <patcharavtk@Patcharas-MacBook-Pro.local>
2014-12-02 16:02:33 +0700

    realfinish

--- lib/lovemac.rb
+++ lib/lovemac.rb
@@ -1,21 +1,29 @@
 def lovemac(input)
-
+	#INITIAL VALUE SET
+	a =0
+	b =0
+	c =0
 	for x in 1..input
-# looping by using x as a condition (check divisble by mod = 0)
-	if x % 3 == 0 
-			if x % 5 == 0
+		# looping by using x to mod to check if its divisible
+		if x % 3 == 0
+		if x % 5 == 0
 				puts "HateWindows"
+				a = a + 1
 			else
 				puts "Love"
+				b = b + 1
 			end
 		elsif x % 5 == 0
  			puts "Mac."
+ 			c = c + 1
 		else
+			puts " "
    			puts "#{x}" 
    		end
+
 	end
-
-	return input
-
+	puts " "
+	puts " "
+	
+	return "("+b.to_s+","+c.to_s+","+a.to_s+")"
 end
-
